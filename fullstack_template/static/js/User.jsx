d285a51b4c314927c1c96f0aa6b9178692a731e2
Shawna Huang <shawna_huang@brown.edu>
2018-01-28 00:48:10 -0500

    sql things!

--- fullstack_template/static/js/User.jsx
+++ fullstack_template/static/js/User.jsx
@@ -6,16 +6,17 @@ class User extends React.Component {
 	    super(props);
 	    this.state = {
 	        username: this.props.username,
-	        course: this.props.course
+	        course: this.props.course,
+	        distance: this.props.distance
 	    }
 	  }
 
 	render () {
         return (
-        <div id="me">
-        <h1>Gaggle</h1>
+        <div className="user">
         <h2>{this.state.username}</h2>
         <p>studying {this.state.course}</p>
+        <p>{this.state.distance} mi away</p>
         </div>
         );
     }
--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 # server.py
 from flask import Flask, render_template, request
 
-import re, sqlite3
+import re, sqlite3, traceback
 
 app = Flask(__name__, static_folder="fullstack_template/static/dist", \
 	template_folder="fullstack_template/static")
@@ -32,22 +32,41 @@ def driver():
 	try:
 		with sqlite3.connect("database.db") as con:
 			cur = con.cursor()
-			cur.execute("INSERT INTO users (username, course, lat, lon) VALUES (?, ?, ?, ?)", (username, course, lat, lon))
+			cur.execute("INSERT INTO users (username, course, lat, lon) VALUES (?, ?, ?, ?)",
+			 (username, course, lat, lon))
 			con.commit()
 			print("User successfully added")
-	except:
+	except Exception as e:
+		if e == "UNIQUE constraint failed: user.username":
+			cur.execute("UPDATE users SET course = ?, lat = ?, lon = ? WHERE username = ?",
+			 (course, lat, lon, username))
 		con.rollback()
-		print("Error in insert operation")
+		print(e)
 
 	finally:
-		con.close() 
+		con.close()
+
+	try:
+		with sqlite3.connect("database.db") as con:
+			cur = con.cursor()			
+			cur.execute("SELECT * FROM users WHERE course = ? AND username != ?", 
+				(course, username))
+			rows = cur.fetchall()
+
+	except Exception as e:
+		con.rollback()
+		print(e)
+	finally:
+		con.close()
+
+
 
 	return username + "," + course
 
 @app.before_first_request
 def make_db():
 	conn = sqlite3.connect('database.db')
-	conn.execute('CREATE TABLE IF NOT EXISTS users (username TEXT PRIMARY KEY, course TEXT, lat DECIMAL, lon DECIMAL)')
+	conn.execute('CREATE TABLE IF NOT EXISTS users (username TEXT, course TEXT, lat DECIMAL, lon DECIMAL)')
 	conn.close()
 
 if __name__ == "__main__":
