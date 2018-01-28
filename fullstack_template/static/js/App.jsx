3fb94526535327f32a5befc075a82ae16a449f1b
Karen <karen_tu@brown.edu>
2018-01-28 09:22:03 -0500

    redundant changes

--- fullstack_template/static/js/App.jsx
+++ fullstack_template/static/js/App.jsx
@@ -6,7 +6,7 @@ class App extends React.Component {
     return (
     	<div>
     		<div className='header'>
-                <img src="images/gooselogo.png" className='logo' />
+                <img src="gooselogo.png" className='logo' />
     		</div>
     		<Form />
     	</div>
